1b9b1247e4c039afd5bbf9cd4d10fd298b289482
krola1 <kwl082024@gmail.com>
2024-10-01 13:39:56 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RECURSIVE_TEST;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        recTest rec = new recTest();
+    }
+}
+
---
+++ classes/recTest.cs
@@ -0,0 +1,15 @@
+public class recTest
+{
+
+    public int RecursiveFactorial(int n)
+    {
+        // Todo: Implement a recursive method(a method that calls itself within its own function scope)
+        // To Check the factorial of a number(norsk: Fakultet) we look at n!, where n = n * 1 * 2 * 3...
+        if (n == 0)
+        {
+            return 1;
+        }
+
+        return n * RecursiveFactorial(n - 1);
+    }
+}
